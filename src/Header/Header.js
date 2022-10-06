eca9e96fd135e734c784d7dcb09abf6088e0678d
m-ghiringhelli <mghiring@gmail.com>
2022-10-06 14:44:50 -0700

    create header component and initial style

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>I'M A HEADER</h1>
+    </header>
+  )
+}
+
+export default Header
